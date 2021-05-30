99223a16665b803d6db42fe4e416acc33d915689
Shivam <devshivam1996@gmail.com>
2021-05-30 22:54:35 +0530

    Created aws elastic transcoder configuration file

---
+++ configuration/transcoderConfiguration.js
@@ -0,0 +1,18 @@
+const transcoderConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_BUCKET_REGION,
+  videoBucket: process.env.AWS_BUCKET_NAME,
+  transcode: {
+    video: {
+      pipelineId: process.env.AWS_PIPELINE_ID,
+      outputKeyPrefix: process.env.AWS_OUTPUT_KEY_PREFIX,
+      presets: [
+        {presetId: '1351620000001-000020', suffix: '_480', prefix: 'large_'},
+        {presetId: '1351620000001-000040', suffix: '_360', prefix: 'medium_'},
+      ],
+    },
+  },
+};
+
+module.exports = transcoderConfig;
