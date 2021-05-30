00f5f08691cb9fc707c2113ecb42387f5a8b3b69
Shivam <devshivam1996@gmail.com>
2021-05-30 22:40:32 +0530

    Rename the database configuration file

--- configuration/databaseConfiguration.js
+++ configuration/databaseConfiguration.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {database} = require('./appConfigurer');
+const {database} = require('./appConfiguration');
 
 const dbConnect = async () => {
   const mongoConfig = {
