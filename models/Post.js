170ed68608a0a264815991be950ecb9535bd41b1
Shivam <devshivam1996@gmail.com>
2021-05-30 21:44:01 +0530

    Created a model file for Post schema

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+  {
+    content: {type: String, trim: true, require: true},
+    media: {type: String, trim: true, required: true},
+    thumbnail: {type: String, trim: true},
+    post_status: {
+      type: String,
+      default: 'draft',
+      enum: ['draft', 'live', 'deleted'],
+    },
+    media_status: {
+      type: String,
+      default: 'processing',
+      enum: ['completed', 'processing'],
+    },
+    deleted_at: {type: Date, default: undefined},
+  },
+  {timestamps: true}
+);
+
+module.exports = mongoose.model('Post', PostSchema);
