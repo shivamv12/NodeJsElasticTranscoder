8dfccacecf170201cb2ae573deb7a4b2cf5f88d5
Shivam <devshivam1996@gmail.com>
2021-06-05 16:40:01 +0530

    Merge the video uploading into transcoder resolver to make it work as asynchronous

--- jobs/resolvers/uploadVideoResolver.js
+++ jobs/resolvers/uploadVideoResolver.js
@@ -1,5 +1,12 @@
 const AWS = require('aws-sdk');
+const Post = require('../../models/Post');
 const transcoderConfig = require('../../configuration/transcoderConfiguration');
+
+/** AWS configuration details */
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 AWS.config.update({
   region: transcoderConfig.region,
@@ -12,50 +19,69 @@ AWS.config.update({
  * @desc {} - Resolving the job of uploading multiple versions of uploaded video.
  */
 const uploadVideoResolver = async (payload) => {
-  const {filename} = payload;
+  const {media, slug, filename} = payload;
 
   const transcoder = new AWS.ElasticTranscoder();
   let ext = filename.split('.').reverse().shift();
   let withoutExt = filename.split('.').reverse().pop();
-  let file_name = withoutExt + '.' + ext;
+  let file_name = `${withoutExt}.${ext}`;
 
-  /** Transcoder Service */
-  let params = {
-    PipelineId: transcoderConfig.transcode.video.pipelineId,
-    Input: {
-      Key: transcoderConfig.transcode.video.outputKeyPrefix + '/' + file_name,
-      FrameRate: 'auto',
-      Resolution: 'auto',
-      AspectRatio: 'auto',
-      Interlaced: 'auto',
-      Container: 'auto',
+  s3.putObject(
+    {
+      ContentType: media.mimetype,
+      Body: Buffer.from(media.data.data),
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `${process.env.AWS_OUTPUT_KEY_PREFIX}/${filename}`,
     },
-    OutputKeyPrefix: transcoderConfig.transcode.video.outputKeyPrefix + '/',
-    Outputs: transcoderConfig.transcode.video.presets.map((p) => {
-      return {
-        PresetId: p.presetId,
-        Key: `${p.prefix}${file_name}`,
-        ThumbnailPattern: `thumb-${p.prefix}${withoutExt}-{count}`,
+    (error, res) => {
+      if (!!error) console.error(`Failed to upload media file: ${error}`);
+
+      console.log(`${JSON.stringify(res)} successfully uploaded.`);
+
+      /** Transcoder Service */
+      let params = {
+        PipelineId: transcoderConfig.transcode.video.pipelineId,
+        Input: {
+          FrameRate: 'auto',
+          Container: 'auto',
+          Interlaced: 'auto',
+          Resolution: 'auto',
+          AspectRatio: 'auto',
+          Key: `${transcoderConfig.transcode.video.outputKeyPrefix}/${file_name}`,
+        },
+        OutputKeyPrefix: `${transcoderConfig.transcode.video.outputKeyPrefix}/`,
+        Outputs: transcoderConfig.transcode.video.presets.map((p) => {
+          return {
+            PresetId: p.presetId,
+            Key: `${p.prefix}${file_name}`,
+            ThumbnailPattern: `thumb-${p.prefix}${withoutExt}-{count}`,
+          };
+        }),
       };
-    }),
-  };
 
-  transcoder.createJob(params, (err, data) => {
-    if (!!err) {
-      console.warn(err);
-      return;
+      transcoder.createJob(params, (error, data) => {
+        if (!!error) console.error(`Failed to create job: ${error}`);
+        const jobId = data.Job.Id;
+        console.info(`AWS transcoder job created (${jobId})`);
+
+        transcoder.waitFor('jobComplete', {Id: jobId}, async (error, data) => {
+          if (!!error)
+            console.error(`Failed to wait for job complete: ${error}`);
+
+          console.log(`AWS transcoder job completed (${jobId})`);
+          await Post.findOneAndUpdate(
+            {slug: slug},
+            {
+              $set: {
+                media_status: 'completed',
+                thumbnail: `thumb-large-${withoutExt}-00001.png`,
+              },
+            }
+          );
+        });
+      });
     }
-    let jobId = data.Job.Id;
-    console.info('AWS transcoder job created (' + jobId + ')');
-
-    transcoder.waitFor('jobComplete', {Id: jobId}, (err, data) => {
-      if (!!err) console.log('Failed to wait for job complete: ', err);
-      else {
-        console.log('AWS transcoder job completed (' + jobId + ')');
-        console.log('\nNeed to update media_statue & thumbnail.');
-      }
-    });
-  });
+  );
 };
 
 module.exports = uploadVideoResolver;
--- services/appService.js
+++ services/appService.js
@@ -1,19 +1,20 @@
 const Post = require('../models/Post');
-const uploadFile = require('../utils/fileUploader');
 const uploadVideoJob = require('../jobs/uploadVideoJob');
+const slugGenerator = require('../helpers/slugGenerator');
 const mediaNameGenerator = require('../helpers/mediaNameGenerator');
 
-const submitPost = async (payload, media) => {
+const submitPost = async (content, media) => {
   let postData = new Post();
-  postData.content = payload.content;
+  postData.content = content;
   postData.media = await mediaNameGenerator(media.name);
+  postData.slug = await slugGenerator(content.slice(0, 15), {length: 6});
   postData = await postData.save();
 
   /** Upload original video */
-  await uploadFile(postData.media, media.data, media.mimetype);
+  // await uploadFile(postData.media, media.data, media.mimetype);
 
-  /** Spinning off redis job to upload different versions */
-  uploadVideoJob({filename: postData.media});
+  /** Spinning off redis job to upload video & their different formats */
+  uploadVideoJob({media, slug: postData.slug, filename: postData.media});
 
   return postData;
 };
